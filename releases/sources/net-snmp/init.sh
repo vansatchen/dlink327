a42b0e682025c2b15e98cc88894e35929f6ba944
Nikolay <rusatch@gmail.com>
2014-11-08 14:06:53 +0500

    fix ldconfig scan at startup

--- releases/sources/net-snmp/init.sh
+++ releases/sources/net-snmp/init.sh
@@ -4,8 +4,9 @@ path=$1
 
 # Check that ldconfig is exists
 if ! which ldconfig >/dev/null; then
-        cp $path/sbin/ldconfig /sbin/ && /sbin/ldconfig $path/lib
+        cp $path/sbin/ldconfig /sbin/
 fi
+/sbin/ldconfig $path/lib
 
 [ -f `which snmpd` ] && rm -f `which snmpd`
 ln -s $path/sbin/snmpd /bin/
