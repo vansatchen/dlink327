4dec9afb33dabf25e55b43df651e6ba449b30f91
Nikolay <rusatch@gmail.com>
2014-12-02 12:11:02 +0500

    added iperf & iperf3

--- releases/sources/term-utils/clean.sh
+++ releases/sources/term-utils/clean.sh
@@ -6,5 +6,7 @@ rm -f /bin/ifstat
 rm -f /bin/tcpdump
 rm -f /bin/trafshow
 rm -f /etc/trafshow
+rm -f /bin/iperf
+rm -f /bin/iperf3
 
 exit 0
--- releases/sources/term-utils/init.sh
+++ releases/sources/term-utils/init.sh
@@ -12,5 +12,7 @@ fi
 [ -f `which ifstat` ] && rm -f `which ifstat`
 [ -f `which tcpdump` ] && rm -f `which tcpdump`
 [ -f `which trafshow` ] && rm -f `which trafshow`
+[ -f `which iperf` ] && rm -f `which iperf`
+[ -f `which iperf3` ] && rm -f `which iperf3`
 
 exit 0
