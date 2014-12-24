023163ff872836ced9f77bc23253c132618eec6a
Nikolay <rusatch@gmail.com>
2014-12-24 12:34:24 +0500

    to Polipo added web-interface allowing from nas-admin-interface

--- releases/sources/Polipo/clean.sh
+++ releases/sources/Polipo/clean.sh
@@ -3,5 +3,6 @@
 #remove links
 rm -f /bin/polipo
 rm -f /etc/polipo
+rm -f /var/www/Polipo
 
 exit 0
--- releases/sources/Polipo/init.sh
+++ releases/sources/Polipo/init.sh
@@ -4,5 +4,6 @@ path=$1
 
 [ -f /bin/polipo ] && rm -f /bin/polipo || cp $path/bin/polipo /bin/
 [ -f /etc/polipo ] && rm -f /etc/polipo || ln -s $path/etc /etc/polipo
+[ -f /var/www/Polipo ] && rm -f /var/www/Polipo || ln -s $path/web /var/www/Polipo
 
 exit 0
