6c81891181873c8a5a2a2179ac48c82944258fc8
Nikolay <rusatch@gmail.com>
2015-01-21 17:25:29 +0500

    fixes generating xml

--- releases/sources/MyKODI/start.sh
+++ releases/sources/MyKODI/start.sh
@@ -5,7 +5,7 @@ ln -s $path/database/kodi /mnt/HD_a4/.@database@/
 mysqlmgr -c -u kodi -pkodipassword -n kodi
 
 mysql -B --disable-column-names -u root -pXP4VddgD0zd8IbKQ -e "SELECT USER,host from mysql.user;" | grep kodi | grep % &> /dev/null
-[ $? -eq 0 ] && exit 0 || mysql -u root -pXP4VddgD0zd8IbKQ -e "GRANT ALL PRIVILEGES ON kodi.* TO 'kodi'@'%' IDENTIFIED BY 'kodipassword' WITH GRANT OPTION;"
+[ $? -eq 0 ] || mysql -u root -pXP4VddgD0zd8IbKQ -e "GRANT ALL PRIVILEGES ON kodi.* TO 'kodi'@'%' IDENTIFIED BY 'kodipassword' WITH GRANT OPTION;"
 
 EGIGAIP=`ifconfig egiga0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 cat <<EOF > $path/web/advancedsettings.xml
