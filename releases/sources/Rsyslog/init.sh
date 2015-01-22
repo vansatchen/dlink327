63a5664669d9d45c2ec4698cc34729dddf36e335
Nikolay Pavlov <rusatch@gmail.com>
2015-01-22 20:26:45 +0500

    fix link for rsyslogd

--- releases/sources/Rsyslog/init.sh
+++ releases/sources/Rsyslog/init.sh
@@ -2,7 +2,7 @@
 
 path=$1
 
-[ -f /usr/sbin/rsyslogd ] && rm -f /usr/sbin/rsyslogd || cp $path/sbin/rsyslogd /usr/sbin/
+[ -f /usr/sbin/rsyslogd ] && rm -f /usr/sbin/rsyslogd || ln -s $path/sbin/rsyslogd /usr/sbin/
 [ -f /etc/rsyslog.conf ] && rm -f /etc/rsyslog.conf || ln -s $path/etc/rsyslog.conf /etc/
 [ -f /etc/rsyslog.d ] && rm -f /etc/rsyslog.d || ln -s $path/etc/rsyslog.d /etc/
 [ -f /var/www/Rsyslog ] && rm -f /var/www/Rsyslog || ln -s $path/web /var/www/Rsyslog
