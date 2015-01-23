23296dd57f19f258e823f3076846303b2d252c7a
Nikolay <rusatch@gmail.com>
2015-01-23 15:34:38 +0500

    some unusable changes

--- releases/sources/Rsyslog/remove.sh
+++ releases/sources/Rsyslog/remove.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 path=$1
+#BACKUPFOLDER="/mnt/HD_a4/.systemfile/rsyslog"
+
+#[ -d $BACKUPFOLDER ] && rm -rf $BACKUPFOLDER
+#mkdir $BACKUPFOLDER
+#cp -r $path/etc $BACKUPFOLDER
 
 rm -rf $path
 
