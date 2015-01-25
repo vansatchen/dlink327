c5f1cba162221f020943dc5ba73e459a14dc6245
Nikolay Pavlov <rusatch@gmail.com>
2015-01-25 16:18:04 +0500

    update net-snmp to 5.7.3; backup config net-snmp

--- releases/sources/net-snmp/install.sh
+++ releases/sources/net-snmp/install.sh
@@ -5,4 +5,9 @@ path_des=$2
 
 cp -R $path_src $path_des
 
+BACKUPFOLDER="/mnt/HD_a4/.systemfile/.net-snmp"
+
+[ -d $BACKUPFOLDER ] && rm -rf $path_des/net-snmp/etc
+cp -r $BACKUPFOLDER/etc $path_des/net-snmp/etc
+
 exit 0
--- releases/sources/net-snmp/remove.sh
+++ releases/sources/net-snmp/remove.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 path=$1
+BACKUPFOLDER="/mnt/HD_a4/.systemfile/.net-snmp"
+
+[ -d $BACKUPFOLDER ] && rm -rf $BACKUPFOLDER
+mkdir $BACKUPFOLDER
+cp -r $path/etc $BACKUPFOLDER
 
 rm -rf $path
 
