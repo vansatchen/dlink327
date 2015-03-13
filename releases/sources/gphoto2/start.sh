5e7752a7977b972d26e7837d6e82ab89abdcceaf
Nikolay Pavlov <rusatch@gmail.com>
2015-03-13 22:34:15 +0500

    added libs for ports & cameras

--- releases/sources/gphoto2/start.sh
+++ releases/sources/gphoto2/start.sh
@@ -3,6 +3,7 @@
 path=$1
 
 insmod $path/modules/exfat.ko
+[ -f /usr/sbin/gphoto2 ] && rm -f /usr/sbin/gphoto2
 ln -s $path/bin/gphoto2 /bin/
 
 exit 0
--- releases/sources/gphoto2/stop.sh
+++ releases/sources/gphoto2/stop.sh
@@ -2,5 +2,6 @@
 
 rmmod exfat
 [ -f /bin/gphoto2 ] && rm -f /bin/gphoto2
+ln -s /usr/local/modules/sbin/gphoto2 /usr/sbin/gphoto2
 
 exit 0
