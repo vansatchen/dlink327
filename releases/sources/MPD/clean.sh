975d3a08271de9176c2d03fad0c6b7886194d1b4
vansatchen <rusatch@gmail.com>
2015-05-02 01:10:18 +0500

    added mpc & ncmpc

--- releases/sources/MPD/clean.sh
+++ releases/sources/MPD/clean.sh
@@ -3,5 +3,7 @@
 #remove links
 rm -f /bin/mpd
 rm -f /etc/mpd.conf
+rm -f /bin/mpc
+rm -f /bin/ncmpc
 
 exit 0
--- releases/sources/MPD/init.sh
+++ releases/sources/MPD/init.sh
@@ -12,5 +12,15 @@ done
 
 [ -f /bin/mpd ] && rm -f /bin/mpd || ln -s $path/bin/mpd /bin/
 [ -f /etc/mpd.conf ] && rm -f /etc/mpd.conf || ln -s $path/etc/mpd.conf /etc/
+[ -f /bin/mpc ] && rm -f /bin/mpc || ln -s $path/bin/mpc /bin/
+[ -f /bin/ncmpc ] && rm -f /bin/ncmpc || ln -s $path/bin/ncmpc /bin/
+
+#create Library Dir
+if [ ! -d /mnt/HD/HD_a2/MPD_library ] ; then
+    mkdir -p /mnt/HD/HD_a2/MPD_library/Music
+    mkdir -p /mnt/HD/HD_a2/MPD_library/Playlists
+    chown -R nobody:allaccount /mnt/HD/HD_a2/MPD_library
+    chmod -R 777 /mnt/HD/HD_a2/MPD_library
+fi
 
 exit 0
