26df3e32072841717271bc184dd8e3ec9c960cb8
vansatchen <rusatch@gmail.com>
2015-10-03 20:28:45 +0500

    added support for kernel 3.2.34 in usbserial-support

--- releases/sources/usbserial-support/start.sh
+++ releases/sources/usbserial-support/start.sh
@@ -2,7 +2,13 @@
 
 path=$1
 
-insmod $path/modules/usbserial.ko
-insmod $path/modules/pl2303.ko
+#insmod $path/modules/usbserial.ko
+#insmod $path/modules/pl2303.ko
+
+KVERS=`uname -r`
+
+[ $KVERS == "3.2.34" ] && modulespath=modules-3.2.34 || modulespath=modules
+insmod $path/$modulespath/usbserial.ko
+insmod $path/$modulespath/pl2303.ko
 
 exit 0
