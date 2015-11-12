5b424d2a624f656fe82508009be06743808c23eb
vansatchen <rusatch@gmail.com>
2015-11-13 00:19:39 +0500

    fix showing status in web page

--- releases/sources/HID-support/web/scripts/status.php
+++ releases/sources/HID-support/web/scripts/status.php
@@ -8,7 +8,9 @@ if (!$return) {
     echo "<b><font color='red'>HID-device not founded!!!</font></b>";
 }
 
-exec("grep Product -r /proc/bus/usb/devices | grep -i -E 'mouse|keyboard' | sed 's/S:  Product=//g'", $output);
+//exec("grep Product -r /proc/bus/usb/devices | grep -i -E 'mouse|keyboard' | sed 's/S:  Product=//g'", $output);
+//exec("grep Name -r /proc/bus/input/devices | grep -i -E 'mouse|keyboard' | sed 's/N: Name=//g' | sed 's/\"//g'", $output);
+exec("./status.sh", $output);
 //exec("grep -i -E 'Product|Manufacturer' -r /proc/bus/usb/devices | sed -r 's/^[^=]+//g' | sed 's/=//g' | sed -r 'N;s/\n/ /'", $output);
 //exec("grep -i -E 'Product|Manufacturer' -r /proc/bus/usb/devices | sed 's/.*=\(.*\)$/\1/g' | sed 'N;s/\n/ /' | grep -E -i 'mouse|keyboard'", $output);
 //exec("grep -i -E 'Product|Manufacturer' -r /proc/bus/usb/devices | sed 'N;s/\n//g'", $output);
---
+++ releases/sources/HID-support/web/scripts/status.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#grep Name -r /proc/bus/input/devices | grep -i -E 'mouse|keyboard' | sed 's/.*=\(.*\)$/\1/g' | sed 's/\"//g'
+grep -E -i 'Name|Handlers' -r /proc/bus/input/devices | sed 's/.*=\(.*\)$/\1/g; s/\"//g' | sed 'N;s/\n/ /'
