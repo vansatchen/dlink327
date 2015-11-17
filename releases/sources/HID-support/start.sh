4c9693e2e621264163c3bacbcfcb1c360adad220
Nikolay <rusatch@gmail.com>
2015-11-17 15:29:22 +0500

    added autostart enabled leds after reboot

--- releases/sources/HID-support/start.sh
+++ releases/sources/HID-support/start.sh
@@ -10,4 +10,11 @@ insmod $path/modules/usbhid.ko
 sleep 3
 sh $path/web/scripts/findevent.sh
 
+ledstatefile=`cat $path/web/ledstate | sed 's/{//g;s/}//g;s/"//g'`
+
+for i in 1 2 3; do
+        buttonstate=`echo $ledstatefile | cut -d ',' -f $i | sed 's/:/_/g;s/true/on/g;s/false/off/g'`
+        /usr/bin/curl http://127.0.0.1/cgi-bin/ctlrelay.cgi?command=$buttonstate
+done
+
 exit 0
