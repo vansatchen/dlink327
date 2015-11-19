6d729a3bbd6738d8df6a85b7f803911f2da34f84
vansatchen <rusatch@gmail.com>
2015-11-19 20:03:41 +0500

    added resumption state of leds after reboot

--- releases/sources/HID-support/start.sh
+++ releases/sources/HID-support/start.sh
@@ -14,7 +14,7 @@ ledstatefile=`cat $path/web/ledstate | sed 's/{//g;s/}//g;s/"//g'`
 
 for i in 1 2 3; do
         buttonstate=`echo $ledstatefile | cut -d ',' -f $i | sed 's/:/_/g;s/true/on/g;s/false/off/g'`
-        /usr/bin/curl http://127.0.0.1/cgi-bin/ctlrelay.cgi?command=$buttonstate
+        $path/web/scripts/ctlrelay $buttonstate
 done
 
 exit 0
