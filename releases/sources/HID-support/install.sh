b32c01d179301a5359f2ba65c40af7aea42061c9
vansatchen <rusatch@gmail.com>
2015-11-20 17:32:07 +0500

    fix branch

--- releases/sources/HID-support/install.sh
+++ releases/sources/HID-support/install.sh
@@ -5,4 +5,11 @@ path_des=$2
 
 cp -R $path_src $path_des
 
+if [ -d /mnt/HD_a4/.systemfile/.HID-support ]; then
+	rm -f /mnt/HD/HD_a2/Nas_Prog/HID-support/web/relayctl/index.html && rm -f /mnt/HD/HD_a2/Nas_Prog/HID-support/web/ledstate
+	cp /mnt/HD_a4/.systemfile/.HID-support/index.html /mnt/HD/HD_a2/Nas_Prog/HID-support/web/relayctl/
+	cp /mnt/HD_a4/.systemfile/.HID-support/ledstate /mnt/HD/HD_a2/Nas_Prog/HID-support/web/
+	rm -rf rm -rf /mnt/HD_a4/.systemfile/.HID-support
+fi
+
 exit 0
--- releases/sources/HID-support/remove.sh
+++ releases/sources/HID-support/remove.sh
@@ -2,6 +2,11 @@
 
 path=$1
 
+[ -d /mnt/HD_a4/.systemfile/.HID-support ] && rm -rf /mnt/HD_a4/.systemfile/.HID-support
+mkdir -p /mnt/HD_a4/.systemfile/.HID-support
+cp $path/web/relayctl/index.html /mnt/HD_a4/.systemfile/.HID-support/
+cp $path/web/ledstate /mnt/HD_a4/.systemfile/.HID-support/
+
 rm -rf $path
 
 exit 0
